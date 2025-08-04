a261a64f0666079b4cfe7e6495c24fcb5144f571
neruteto <neruteto@0yap.com>
2025-08-04 17:02:36 +0200

    Create fixModTime.sh

---
+++ fixModTime.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Loop through all files in the current directory
+for file in $(git ls-files); do
+    # Get the last commit date for the file
+    commit_date=$(git log -1 --format="%ct" -- "$file")
+    
+    # Check if commit_date is not empty
+    if [ -n "$commit_date" ]; then
+        # Update the file's last modified timestamp
+        touch -d @"$commit_date" "$file"
+    else
+        echo "No commit date found for $file"
+    fi
+done
